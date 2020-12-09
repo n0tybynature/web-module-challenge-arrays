56d373890f84089028099fd519256ca0b322fada
Tony Yang <tonyandbullet@gmail.com>
2020-12-09 15:30:22 -0800

    tried to skretch

--- index.js
+++ index.js
@@ -197,8 +197,14 @@ Use the getAverageWordLength function below to do the following:
     For example: getAverageWordLength(originalFlavors) should return a number between 0 and 3.     
 */
 
-function getAverageWordLength(/*code here*/){
-    /*code here*/
+function getAverageWordLength(originalFlavors){
+    const newave = [];
+    for (let i = 0; i < originalFlavors.length; i++){
+        if (originalFlavors.charAt(i)===""){
+            count++;
+        }
+    }
+    return getAverageWordLength;
 }
 
 
